2682b40399fdc33dd7a11cb62f2a88de5f40ae3f
Nastya <nastya.drzh@gmail.com>
2016-09-02 12:39:33 +0300

    code refactoring

--- friends-app-domain/src/main/java/com/cheliadina/domain/Place.java
+++ friends-app-domain/src/main/java/com/cheliadina/domain/Place.java
@@ -41,4 +41,22 @@ public class Place {
         this.title = title;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Place place = (Place) o;
+
+        if (id != place.id) return false;
+        return title != null ? title.equals(place.title) : place.title == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
 }
